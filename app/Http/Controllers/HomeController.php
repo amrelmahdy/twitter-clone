960c8ca5143fb9f3c9e59656e245397674347bb6
Amr El Mahdy <amrelmhdy@hotmail.com>
2018-05-12 16:03:13 +0200

    add datbase to follder database-staff

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,21 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Follow;
-use App\Models\User;
-use Carbon\Carbon;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Session;
-use Validator;
-use Super;
-use Auth;
-
 class HomeController extends Controller
 {
     public function index()
     {
-        dd(Super::pusher());
-
         return view('pages.index');
     }
 
